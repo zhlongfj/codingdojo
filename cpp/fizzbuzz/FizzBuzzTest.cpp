cb23f7b5043c70da7b2f9e6c5025e15b464329ae
zhlongfj <zhlongfj@sina.com>
2021-01-25 13:59:08 +0800

    red

--- cpp/fizzbuzz/FizzBuzzTest.cpp
+++ cpp/fizzbuzz/FizzBuzzTest.cpp
@@ -10,11 +10,17 @@ SCENARIO("FizzBuzz", "FizzBuzz") {
         WHEN("divisable by 3") {
             CHECK(FizzBuzz(3).numberOff() == "Fizz");
         }
+
         WHEN("divisable by 5") {
             CHECK(FizzBuzz(5).numberOff() == "Buzz");
         }
+
         WHEN("divisable by 7") {
             CHECK(FizzBuzz(7).numberOff() == "Whizz");
+        }
+
+        WHEN("divisable by 3 and 5") {
+            CHECK(FizzBuzz(15).numberOff() == "FizzBuzz");
         }
     }
 }
