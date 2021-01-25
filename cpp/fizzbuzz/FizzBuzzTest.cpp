7d7c014eb21a99543b7a2cb0553a4619aa139058
zhlongfj <zhlongfj@sina.com>
2021-01-25 13:49:41 +0800

    write test: divisable by 3

--- cpp/fizzbuzz/FizzBuzzTest.cpp
+++ cpp/fizzbuzz/FizzBuzzTest.cpp
@@ -4,3 +4,12 @@
 #define CATCH_CONFIG_MAIN
 #include "../catch.hpp"
 
+SCENARIO("FizzBuzz", "FizzBuzz") {
+    GIVEN("given an number") {
+        WHEN("divisable by 3") {
+            FizzBuzz fizzBuzz;
+            std::string number = fizzBuzz.numberOff();
+            CHECK(number == "Fizz");
+        }
+    }
+}
